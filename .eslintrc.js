4602a549fb60641daff3043b8b5411475fee57e8
Thalissa Jenniffer <thalissajenniffer@hotmail.com>
2018-09-05 02:54:08 -0300

    lint inserido

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "commonjs": true,
+        "es6": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": 2017
+    },
+    "rules": {
+        "indent": [
+            "error",
+            "tab"
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "double"
+        ],
+        "semi": [
+            "error",
+            "never"
+        ]
+    }
+};
